39e864b0a2a7911ace873991ca708a3849ccc170
Thomas van Erven <thomasvanerven@DANSs-MacBook-Pro-2.local>
2022-04-21 13:13:40 +0200

    Typo.

--- start-prod.sh
+++ start-prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 docker-compose --env-file .env.prod -f docker-compose-ssl-proxy.yaml up -d
-docker-compose --env-file .env.prod -f docker-compose-ssl-services.yaml up -d
+docker-compose --env-file .env.prod -f docker-compose-ssl-service.yaml up -d
