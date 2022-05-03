9cfb8e1ec02e3b20f0f4dce69986509444e30350
Thomas van Erven <thomasvanerven@danss-mbp-2.kpn>
2022-05-03 16:28:59 +0200

    Loading more Skosmos dicts.

--- odissei-init.sh
+++ odissei-init.sh
@@ -10,5 +10,13 @@ curl -I -X POST -H Content-Type:text/turtle -T unescothes.ttl -G http://0.0.0.0:
 # wget from github and store CBS vocabulary in ./config/cbs-variables-thesaurus.ttl
 curl -I -X POST -H Content-Type:text/turtle -T ./config/cbs-variables-thesaurus.ttl -G http://0.0.0.0:3030/skosmos/data --data-urlencode graph=http://cbs.nl/variables/
 
+# ellst
+curl -I -X POST -H Content-Type:text/turtle -T ./config/ELLST_2021.ttl -G http://0.0.0.0:3030/skosmos/data --data-urlencode graph=http://thesauri.cessda.eu/ellst/
+
+# cessda general data format
+curl -I -X POST -H Content-Type:text/rdf -T ./config/cessda_general_data_format.ttl -G http://0.0.0.0:3030/skosmos/data --data-urlencode graph=https://vocabularies.cessda.eu/vocabulary/GeneralDataFormat
+
+# cessda topic classification
+curl -I -X POST -H Content-Type:text/rdf -T ./config/cessda_topic.ttl -G http://0.0.0.0:3030/skosmos/data --data-urlencode graph=https://vocabularies.cessda.eu/vocabulary/TopicClassification
 echo "Checking search index..."
 curl "http://0.0.0.0:8000/rest/v1/search?vocab=stw&query=a*"
